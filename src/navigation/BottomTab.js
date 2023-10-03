b02ead1c11e8f0ec559daeba2de12b98a98d30f9
oliverkean <20215296@s.ubaguio.edu>
2023-10-03 15:33:55 +0800

    bottomtabs

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TodoScreen from '../screen/TodoScreen';
 import AddListScreen from '../screen/AddListScreen';
+import AnalyticsScreen from '../screen/AnalyticsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ export default function BottomTab() {
     <Tab.Navigator initialRouteName="Home">
       <Tab.Screen name="Home" component={TodoScreen} />
       <Tab.Screen name="Add Assignment" component={AddListScreen} />
+      <Tab.Screen name="Analytics" component={AnalyticsScreen} />
     </Tab.Navigator>
   )
 }
